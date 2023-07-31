b3aec60dff192c33f119d6d3eee88416fe7baafe
2HO4 <khoapham2905@gmail.com>
2023-07-31 14:03:52 +0200

    initial commit

---
+++ dashboards/config.py
@@ -0,0 +1,34 @@
+# TWITTER CONFIGURATION
+# ----------------------------------------------------------------------------------------------------------------
+
+
+# INITIATION ------------
+
+
+# EXECUTION -------------
+
+TWITTER_CONSUMER_KEY = 'dJ6yJGAP8DZRrtoJm3B5siy7j'
+TWITTER_CONSUMER_SECRET = 'r1rI8kFHKRJVbrzkZlBoVD9gcchWHiVtvZPQJTQesVc3UPx15U'
+TWITTER_ACCESS_TOKEN = '863652138961952768-RLBNe2Y3O1McVoe3BcOrVoEgQohiOUv'
+TWITTER_ACCESS_TOKEN_SECRET = 'lj1YPUk3kN9zwGTRqh42LRHA2Pydpny0PBbaR7Ej8lQkZ'
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'cancuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
+
+
+# DRAFT -----------------
+
+"""
+
+
+
+"""
